5aa2af381fbfe1fd25eca7a208db2223ca250813
李存昌 <mail@licunchang.com>
2017-11-27 10:07:46 +0800

    验证prefix

--- setting/web.go
+++ setting/web.go
@@ -1,5 +1,13 @@
 package setting
 
+import (
+	"net/url"
+	"strings"
+
+	"chuanyun.io/esmeralda/util"
+	"github.com/sirupsen/logrus"
+)
+
 type WebSettings struct {
 	Port   int64
 	Prefix string
@@ -7,6 +15,21 @@ type WebSettings struct {
 }
 
 func ValidateWebSettings() {
+
+	u, err := url.Parse("http://localhost/" + Settings.Web.Prefix)
+	if err != nil {
+		logrus.Fatal(util.Message("Web prefix path error"))
+	}
+
+	Settings.Web.Prefix = u.Path
+
+	if strings.HasSuffix(Settings.Web.Prefix, "/") {
+		Settings.Web.Prefix = strings.TrimSuffix(Settings.Web.Prefix, "/")
+	}
+	if !strings.HasPrefix(Settings.Web.Prefix, "/") {
+		Settings.Web.Prefix = "/" + Settings.Web.Prefix
+	}
+
 	if Settings.Web.Prefix == "/" {
 		Settings.Web.Prefix = ""
 	}
