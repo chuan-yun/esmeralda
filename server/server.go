e383b9887052fd27a83b9da36655d9ff775fb560
李存昌 <mail@licunchang.com>
2017-11-27 15:18:53 +0800

    添加服务器信息

--- server/server.go
+++ server/server.go
@@ -142,10 +142,9 @@ func (me *HttpServer) Start(ctx context.Context) (err error) {
 	listenAddr := fmt.Sprintf("%s:%s", setting.Settings.Web.Address, strconv.FormatInt(setting.Settings.Web.Port, 10))
 
 	router := httprouter.New()
-	router.GET(setting.Settings.Web.Prefix+"/", controller.Index)
 	router.GET(setting.Settings.Web.Prefix+"/collector/log", controller.Collect)
 
-	router.Handler("GET", setting.Settings.Web.Prefix+"/exporter/metrics", promhttp.Handler())
+	router.Handler("GET", setting.Settings.Web.Prefix+"/metrics", promhttp.Handler())
 
 	me.httpSrv = &http.Server{Addr: listenAddr, Handler: router}
 
