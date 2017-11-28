732dd0261d2151156d7a773f62352ecc88f7fcde
李存昌 <mail@licunchang.com>
2017-11-28 10:56:44 +0800

    test

--- controller/collector.go
+++ controller/collector.go
@@ -1,18 +1,26 @@
 package controller
 
 import (
+	"bytes"
 	"net/http"
 
+	"chuanyun.io/esmeralda/collector/trace"
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 )
 
 func Collect(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Write([]byte(`
-		<html>
-			<head><title>Metrics Exporter</title></head>
-			<body>
-				<h1>Metrics Exporter</h1>
-				<p><a href="./metrics">Metrics</a></p>
-			</body>
-		</html>`))
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	body := buf.String()
+
+	spans, spanJSONError := trace.ToSpans(body)
+	if spanJSONError != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": spanJSONError,
+			"trace": body,
+		}).Warn("main: trace log decode to json error")
+	}
+	logrus.Info(spans)
 }
